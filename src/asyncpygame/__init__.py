40008e345c70fd4dcfa74bed987b98da36e94f06
Nattōsai Mitō <flow4re2c@gmail.com>
2024-10-10 21:58:24 +0900

    wip

--- src/asyncpygame/__init__.py
+++ src/asyncpygame/__init__.py
@@ -1,11 +1,11 @@
 __all__ = (
-    'run', 'quit', 'Clock', 'SDLEvent', 'PriorityExecutor',
+    'run', 'quit', 'run_and_record', 'Clock', 'SDLEvent', 'PriorityExecutor',
     'CommonParams', 'capture_current_frame', 'block_input_events',
 )
 
 from asyncgui import *
 from asyncgui_ext.clock import Clock
-from ._runner import run, quit
+from ._runner import run, quit, run_and_record
 from ._sdlevent import SDLEvent
 from ._priority_executor import PriorityExecutor
 from ._utils import CommonParams, capture_current_frame, block_input_events
