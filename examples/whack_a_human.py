0487a0bf1340da6a463b45524146e9c5154fb337
Nattōsai Mitō <flow4re2c@gmail.com>
2024-12-26 13:58:58 +0900

    convert_soundのコードをwhack_a_human内に移す

--- examples/whack_a_human.py
+++ examples/whack_a_human.py
@@ -35,7 +35,24 @@ from _uix.inapp_mouse_cursor import inapp_mouse_cursor
 from _uix.anchor_layout import anchor_layout
 from _uix.ripple_button import ripple_button
 from _uix.modal_dialog import ask_yes_no_question, message_with_spinner
-from _utils.convert_sound import convert_sound
+
+
+def convert_sound(source: bytes, *, in_format="wav", out_format="wav", out_codec="pcm_s16le") -> bytes:
+    '''
+    Converts an audio source to another format using ffmpeg.
+    '''
+    import subprocess
+
+    ffmpeg_cmd = (
+        "ffmpeg",
+        "-f", in_format,
+        "-i", "pipe:0",  # stdin
+        "-f", out_format,
+        "-codec:a", out_codec,
+        "pipe:1",  # stdout
+    )
+    p = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=0)
+    return p.communicate(source)[0]
 
 
 def with_isolated_alpha(s: Surface) -> Surface:
